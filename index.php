82b8fcbe6bd6a75d65154c693cba917d6ea8f8f0
adekanmbiabidemi@gmail.com <adekanmbiabidemi@gmail.com>
2025-10-04 17:06:00 +0100

    Update index.php to show public/private IP and hostname

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
-$metadata_base = "http://169.254.169.254/latest/meta-data/";
-
-$private_ip = file_get_contents($metadata_base . "local-ipv4");
-$public_ip  = file_get_contents($metadata_base . "public-ipv4");
-$hostname   = file_get_contents($metadata_base . "hostname");
+$private_ip = $_SERVER['SERVER_ADDR'];        // Private IP of the instance
+$hostname   = gethostname();                  // Hostname
+$public_ip  = trim(file_get_contents('http://checkip.amazonaws.com/')); // Public IP
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,8 +10,8 @@ $hostname   = file_get_contents($metadata_base . "hostname");
 </head>
 <body>
     <h1>EC2 Instance Info</h1>
-    <p><strong>Private IP:</strong> <?php echo $private_ip; ?></p>
-    <p><strong>Public IP:</strong> <?php echo $public_ip; ?></p>
-    <p><strong>Hostname:</strong> <?php echo $hostname; ?></p>
+    <p><strong>Private IP:</strong> <?= $private_ip ?></p>
+    <p><strong>Public IP:</strong> <?= $public_ip ?></p>
+    <p><strong>Hostname:</strong> <?= $hostname ?></p>
 </body>
 </html>
